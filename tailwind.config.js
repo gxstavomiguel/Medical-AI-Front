0d06a72bbe36365b866bcbf79a37ab9fbf9644ba
Gustavo Miguel <gustavomiguelsilvafc@gmail.com>
2025-12-18 17:39:08 -0300

    chore: atualiza dependências e configurações do TypeScript e Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 export const theme = {
   extend: {
+    colors: {
+      primary: "#19AA79",
+      primaryConstraste: "#13785A",
+
+
+
+
+
+      primaryLight: "#32C997",
+      primaryDark: "#0E7150",
+      secondary: "#1E293B",
+      graySoft: "#F3F4F6",
+    },
     fontFamily: {
       montserrat: ['Montserrat', 'sans-serif'],
     },
